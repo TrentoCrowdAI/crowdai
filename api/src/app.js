09d634ef48f5f7fd8faeff98f99c84b031bcc255
Mattia Pavan <mattia.pavan@studenti.unitn.it>
2018-03-28 12:12:23 +0200

    fake getAllTasksByJobId

--- api/src/app.js
+++ api/src/app.js
@@ -32,7 +32,7 @@ app.use(compress());
 app.use(koaBetterBody());
 
 // google-based authentication protects URLs under /requesters
-app.use(authentication.unless({ path: [/^\/auth/, /^\/workers/] }));
+//app.use(authentication.unless({ path: [/^\/auth/, /^\/workers/] }));
 
 // error-handling middleware
 app.use(async (ctx, next) => {

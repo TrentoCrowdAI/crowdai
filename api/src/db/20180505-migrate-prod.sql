72e6a56b0cff89db60e431cec3284031adfd1343
Jorge Ramírez <jorgeramirez1990@gmail.com>
2018-05-05 22:36:05 +0200

    Add script to migrate prod db
    refs #75
    
    Signed-off-by: Jorge Ramírez <jorgeramirez1990@gmail.com>

---
+++ api/src/db/20180505-migrate-prod.sql
@@ -0,0 +1,19 @@
+﻿-- make requester_id nullable in project table
+
+alter table project alter column requester_id drop not null;
+
+-- add column requester_id to job table
+alter table job add column requester_id bigint;
+
+-- fill in requester_id column based on the value on the project table
+update job j set requester_id = p.requester_id
+from (
+  select * from project 
+) as p
+where j.project_id = p.id;
+
+-- drop column requester_id in table project
+alter table project drop column requester_id;
+
+-- add not null constraint
+alter table job ALTER COLUMN requester_id set not null;
